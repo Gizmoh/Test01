6cad6641a8d4b0ed36ae22f7d39be9d0d0f0b6af
Arturo Reyes <Caesarisnotdead@gmail.com>
2013-09-06 10:27:13 -0400

    added database

--- test01.py
+++ test01.py
@@ -1,8 +1,13 @@
 import sqlalchemy
 import classes
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('sqlite:///:memory:', echo =True)
+engine = create_engine('sqlite:///datos.db', echo =True)
 engine.execute("select 1").scalar()
+Session = sessionmaker(bind=engine)
 derp = classes.user("Mordekaiser")
 print (derp.name)
+session = Session()
+session.add(derp)
+session.commit()
