6654d918ec7c3636eae302cd08de5e6810a3f0f0
Arturo Reyes <Caesarisnotdead@gmail.com>
2013-09-06 10:56:26 -0400

    fixes

--- test01.py
+++ test01.py
@@ -7,12 +7,15 @@ engine = create_engine('sqlite:///datos.db', echo =False)
 engine.execute("select 1").scalar()
 Session = sessionmaker(bind=engine)
 session = Session()
+X = True
 classes.user.metadata.create_all(engine)
+while (X == True):
+	Z = raw_input("Inserte nombre: ")
+	A = classes.user(Z)
+	session.add(A)
+	if Z == "":
+		X = False
+session.commit()
 for row in session.query(classes.user).order_by(classes.user.id):
 	print row.name
 print(session.query(classes.user).count())
-session.add_all([
-	classes.user("Omegadurr"),
-	classes.user("XRebirth"),
-	])
-session.commit()
