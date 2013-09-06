2e9cd9fc40f618978ee0ea2a17d6f4d962e0061c
Arturo Reyes <Caesarisnotdead@gmail.com>
2013-09-06 10:49:17 -0400

    further additions

--- test01.py
+++ test01.py
@@ -7,9 +7,12 @@ engine = create_engine('sqlite:///datos.db', echo =False)
 engine.execute("select 1").scalar()
 Session = sessionmaker(bind=engine)
 session = Session()
+classes.user.metadata.create_all(engine)
+for row in session.query(classes.user).order_by(classes.user.id):
+	print row.name
+print(session.query(classes.user).count())
 session.add_all([
-	classes.user("derp"),
-	classes.user("huehue"),
-	classes.user("nerfPl0x")
+	classes.user("Omegadurr"),
+	classes.user("XRebirth"),
 	])
 session.commit()
