bbaba607eac9a8a162cb7c411a05f2d7c8b74632
Arturo Reyes <Caesarisnotdead@gmail.com>
2013-09-06 10:37:12 -0400

    further testing

--- test01.py
+++ test01.py
@@ -3,11 +3,13 @@ import classes
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('sqlite:///datos.db', echo =True)
+engine = create_engine('sqlite:///datos.db', echo =False)
 engine.execute("select 1").scalar()
 Session = sessionmaker(bind=engine)
-derp = classes.user("Mordekaiser")
-print (derp.name)
 session = Session()
-session.add(derp)
+session.add_all([
+	classes.user("derp"),
+	classes.user("huehue"),
+	classes.user("nerfPl0x")
+	])
 session.commit()
